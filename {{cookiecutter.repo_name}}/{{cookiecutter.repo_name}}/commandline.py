751f2f77b5d2157a005f942ff38f12a8bf1de356
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-03-27 16:44:18 -0700

    Add commandline with "hello" example

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/commandline.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Import modified 'os' module with LC_LANG set so click doesn't complain
+from .os_utils import os  # noqa: F401
+
+import click
+
+settings = dict(help_option_names=['-h', '--help'])
+
+from {{ cookiecutter.repo_name }}.hello import hello
+
+
+
+@click.group(options_metavar='', hello_metavar='<command>',
+             context_settings=settings)
+def cli():
+    """
+    {{ cookiecutter.project_short_description }}
+    """
+    pass
+
+
+cli.add_command(hello, name='hello')
+
+
+if __name__ == "__main__":
+    cli()
