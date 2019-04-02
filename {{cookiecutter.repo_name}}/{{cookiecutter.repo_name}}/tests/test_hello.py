b7d9f5fa9e8404576ab16e43f431152f4aa530fe
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-04-01 17:24:49 -0700

    Fix hello tests

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tests/test_hello.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tests/test_hello.py
@@ -15,26 +15,28 @@ def test_hello():
     from {{ cookiecutter.repo_name }}.hello import hello
 
     runner = CliRunner()
-    result = runner.invoke(hello)
+    result = runner.invoke(hello, input="Rosalind Franklin")
 
     assert result.exit_code == 0
+    assert result.output.count("Hello Rosalind Franklin") == 5
 
 
 def test_hello_name():
     from {{ cookiecutter.repo_name }}.hello import hello
 
     runner = CliRunner()
-    result = runner.invoke(hello, ["--name", "Rosalind", "Franklin"])
+    result = runner.invoke(hello, ["--name", "Rosalind"])
 
     assert result.exit_code == 0
+    assert result.output.count("Hello Rosalind") == 5
 
 
 def test_hello_count():
     from {{ cookiecutter.repo_name }}.hello import hello
 
     runner = CliRunner()
-    result = runner.invoke(hello, ["--count", "10"])
+    result = runner.invoke(hello, ["--count", "10",
+                                   "--name", "Rosalind"])
 
     assert result.exit_code == 0
-    assert result.
-
+    assert result.output.count("Hello Rosalind") == 10
